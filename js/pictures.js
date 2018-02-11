5eb14fca3a94b16dbd9b0940a2d70aa2a42f1529
Yarisson <gemini8811@gmail.com>
2018-02-11 07:54:08 +0300

    правки в код. Массивы через функции

--- js/pictures.js
+++ js/pictures.js
@@ -1,7 +1,12 @@
-var arrayLength = 25;
-var ArrayPhotos = [];
+'use strict';
+
+var pictureTemplate = document.querySelector('#picture-template').content; // Переменная блока темплейт
+
+var GalleryOverlay = document.querySelector('.gallery-overlay').content;
+
 var randomNumber = function (a, b) {
-  var rand = a + Math.round(((b-a) * Math.random()));
+  var rand = a + Math.round(((b - a) * Math.random()));
+  return rand;
 };
 
 var COMMENTS_GENERATION = [
@@ -17,165 +22,56 @@ var Likes = randomNumber(15, 200);
 
 var COMENTS_INDEX = randomNumber(0, 5);
 
-var ArrayPictures = function (arr ,N) {
-  for (var i = 0; i < N - 1; i++) {
-    arr[i] = 'photos/{{i}}.jpg';
-   }
+var PICTURES_URL = [];
+
+var ArrayPhotos = function () {  
+
+  for (var i = 0; i < 24; i++) {
+
+    PICTURES_URL[i] = 'photos/{{i}}.jpg';
+
   }
+  return PICTURES_URL;
+};
 
+var PhotosArray = ArrayPhotos();
 
+var pictures = [];
 
-var PICTURES_URL = ArrayPictures( ArrayPhotos,arrayLength);
+var ArrayPictures = function () {
 
-var pictures = [
-  {
-    url: PICTURES_URL[0],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-},
-  {
-    url: PICTURES_URL[1],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[2],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[3],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[4],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[5],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[6],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[7],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[8],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[9],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[10],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[11],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[12],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[13],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[14],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[15],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[16],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[17],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[18],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[19],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[20],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[21],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[22],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[23],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-  {
-    url: PICTURES_URL[24],
-    likes: Likes,
-    comments: COMMENTS_GENERATION[COMENTS_INDEX]
-  },
-];
+  for (var i = 0; i < 24; i++) {
+    pictures.url[i] = PICTURES_URL[i];
+    pictures.likes[i] = Likes;
+    pictures.comments[i] = COMMENTS_GENERATION[COMENTS_INDEX]; 
+  }
+  return pictures;
+}; 
 
-var renderPicture = function(pictures) {
+var PicturesGeneration = ArrayPictures();
+
+var renderPicture = function (picture) {
 
   var pictureElement = pictureTemplate.cloneNode(true);
 
-  pictureElement.querySelector('img[src]').TextContent = pictures[i].url;
-  pictureElement.querySelector('.picture-comments').TextContent = pictures[i].comments;
-  pictureElement.querySelector('.picture-likes').TextContent = pictures[i].likes;
+  pictureElement.querySelector('imgSrc').value = picture.url;
+  pictureElement.querySelector('.picture-comments').TextContent = picture.comments;
+  pictureElement.querySelector('.picture-likes').TextContent = picture.likes;
 
   return pictureElement;
-}
-
-var pictureTemplate = document.querySelector('#picture-template').content; /.. Переменная блока темплейт ../
+};
 
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < pictures.length; i++) {
+for (var i = 0; i < pictures.length - 1; i++) {
 
   fragment.appendChild(renderPicture(pictures[i]));
 }
 
 document.querySelector('.gallery-overlay').classList.remove('hidden');
-pictureElement.querySelector('.gallery-overlay-image.img[src]').TextContent = pictures[0].url;
-pictureElement.querySelector('.comments-count').TextContent = pictures[0].comments;
-pictureElement.querySelector('.likes-count').TextContent = pictures[0].likes;
+
+GalleryOverlay.cloneNode(true);
+
+GalleryOverlay.querySelector('imgSrc').value = pictures[0].url;
+GalleryOverlay.querySelector('.comments-count').TextContent = pictures[0].comments;
+GalleryOverlay.querySelector('.likes-count').TextContent = pictures[0].likes;
